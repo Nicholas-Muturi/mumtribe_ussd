2b3a0ef34dcae8eff22f36f8820a7ffc111d806f
Nicholas Muturi <nmuturi@uapoldmutual.com>
2020-04-14 21:43:14 +0300

    [chore] replaced switch statements with if to test accuracy

--- index.php
+++ index.php
@@ -9,7 +9,112 @@ function input_logic($input, $phoneNumber){
 	$phoneNumber = $phoneNumber;
 	$response = "";
 
-	switch($input){
+	if($input == ""){
+		$response = "CON Welcome. Select an option below \n";
+		$response .= "1. Register \n";
+		$response .= "2. Get baby name \n";
+		$response .= "3. Check for health professionals \n";
+		$response .= "4. Daily health reminders \n";
+		$response .= "5. Ovulation Calculator \n";
+		$response .= "6. Due date calculator\n";
+		$response .= "7. Get instant ambulance for delivery \n";
+		$response .= "8. Checkout nearest health center \n";
+		$response .= "98. More \n";
+	}
+	else if(preg_match("/1$/",$input)){
+		$response = "CON Enter email address \n";
+	}
+	else if(preg_match("/1[*][a-zA-Z0-9_.+]+@[a-zA-Z0-9]+.[a-zA-Z]+$/",$input)){
+		$response = "CON Enter password \n";
+	}
+	else if(preg_match("/1[*][a-zA-Z0-9_.+]+@[a-zA-Z0-9]+.[a-zA-Z]+[*][a-zA-Z0-9]+$/",$input)){
+		$response = "CON Enter age \n";
+	}
+	else if(preg_match("/1[*][a-zA-Z0-9_.+]+@[a-zA-Z0-9]+.[a-zA-Z]+[*][a-zA-Z0-9]+[*][0-9]+$/",$input)){
+		$exploded = explode("*",$input);
+		$email = $exploded[count($exploded)-3];
+		$pass = $exploded[count($exploded)-2];
+		$age = $exploded[count($exploded)-1];
+		$response = "END Email/Password/Age are " . $email . " / " . $pass . " / " . $age . "\n";
+		
+	}
+	//Menu 2
+	else if(preg_match("/2$/",$input)){
+		$response = "CON Select an option below \n";
+		$response .= "1. Male names \n";
+		$response .= "2. Female names \n";
+		$response .= "00. Back Home \n";
+	}
+	else if(preg_match("/2[*]1$/",$input)){
+		$response = "END Male names \n";
+		$malenames = array("Steve", "Tom", "Andrew", "Greg", "John");
+		for($i=0;$i<count($malenames);$i++){
+			$response .= ($i + 1) . " " . $malenames[$i] . "\n";
+		}
+	}
+	else if(preg_match("/2[*]2$/",$input)){
+		$response = "END Female names \n";
+		$femalenames = array("Jane", "Racheal", "Anna", "Tokyo", "Nairobi");
+		for($i=0;$i<count($femalenames);$i++){
+			$response .= ($i + 1) . " " . $femalenames[$i] . "\n";
+		}
+	}
+	//Menu 3
+	else if(preg_match("/3$/",$input)){
+		$response = "END health professional list here";
+	}
+	//Menu 4
+	else if(preg_match("/4$/",$input)){
+		$response = "CON 1. Subscribe \n";
+		$response .= "2. Unsubscribe \n";
+		$response .= "00. Back Home \n";
+	
+	}
+	else if(preg_match("/4[*]1$/",$input)){
+		$response = "END $phoneNumber is now subscribed to health reminders";		
+	}
+	else if(preg_match("/4[*]2$/",$input)){
+		$response = "END $phoneNumber is now unsubscribed to health reminders";		
+	}
+	//Menu 5
+	else if(preg_match("/5$/",$input)){
+		$response = "CON Enter the date of your last period (01-31) \n";		
+	}
+	else if(preg_match("/5[*][0-3][0-1]+$/",$input)){
+		$response = "CON Enter the month of your last period\n";		
+	}
+	else if(preg_match("/5[*][0-3][0-1][*][a-zA-Z]+$/",$input)){
+		$exploded = explode("*",$input);
+		$day = $exploded[count($exploded)-2];
+		$month = $exploded[count($exploded)-1];
+		$year = date("Y");
+
+		$response = "END Previous cycle:$day/$month/$year. -- Next cycle date: n/a";		
+	}
+	else if(preg_match("/98$/", $input)){
+		$response = "CON Select an option below \n";
+		$response .= "9. Check maternity shops \n";
+		$response .= "10. Book private birthing \n";
+		$response .= "11. Subscribe daily tips\n";
+		$response .= "00. Back Home \n";
+	}
+	else if(preg_match("/[*]00$/", $input)){
+		$response = "CON Welcome. Select an option below \n";
+		$response .= "1. Register \n";
+		$response .= "2. Get baby name \n";
+		$response .= "3. Check for health professionals \n";
+		$response .= "4. Daily health reminders \n";
+		$response .= "5. Ovulation Calculator \n";
+		$response .= "6. Due date calculator\n";
+		$response .= "7. Get instant ambulance for delivery \n";
+		$response .= "8. Checkout nearest health center \n";
+		$response .= "98. More \n";		
+	}
+	else {
+		$response = "END An error occured";
+	}
+
+	/*switch($input){
 		case "":
 			$response = "CON Welcome. Select an option below \n";
 			$response .= "1. Register \n";
@@ -22,26 +127,7 @@ function input_logic($input, $phoneNumber){
 			$response .= "8. Checkout nearest health center \n";
 			$response .= "98. More \n";
 		break;
-		case preg_match("/98$/", $input):
-			$response = "CON Select an option below \n";
-			$response .= "9. Check maternity shops \n";
-			$response .= "10. Book private birthing \n";
-			$response .= "11. Subscribe daily tips\n";
-			$response .= "00. Back Home \n";
-		break;
-		case preg_match("/[*]00$/", $input):
-			$response = "CON Welcome. Select an option below \n";
-			$response .= "1. Register \n";
-			$response .= "2. Get baby name \n";
-			$response .= "3. Check for health professionals \n";
-			$response .= "4. Daily health reminders \n";
-			$response .= "5. Ovulation Calculator \n";
-			$response .= "6. Due date calculator\n";
-			$response .= "7. Get instant ambulance for delivery \n";
-			$response .= "8. Checkout nearest health center \n";
-			$response .= "98. More \n";
-		break;
-
+		
 		//Menu 1
 		case preg_match("/1$/",$input):
 			$response = "CON Enter email address \n";
@@ -158,7 +244,6 @@ function input_logic($input, $phoneNumber){
 			$response = "CON 1. Subscribe \n";
 			$response .= "2. Unsubscribe \n";
 			$response .= "00. Back Home \n";
-
 		break;
 		case preg_match("/11[*]1$/",$input):
 			$response = "END $phoneNumber is now subscribed to daily tips";
@@ -167,13 +252,34 @@ function input_logic($input, $phoneNumber){
 			$response = "END $phoneNumber is now unsubscribed to daily tips";
 		break;
 
+		//Extras
+		case preg_match("/98$/", $input):
+			$response = "CON Select an option below \n";
+			$response .= "9. Check maternity shops \n";
+			$response .= "10. Book private birthing \n";
+			$response .= "11. Subscribe daily tips\n";
+			$response .= "00. Back Home \n";
+		break;
+		case preg_match("/[*]00$/", $input):
+			$response = "CON Welcome. Select an option below \n";
+			$response .= "1. Register \n";
+			$response .= "2. Get baby name \n";
+			$response .= "3. Check for health professionals \n";
+			$response .= "4. Daily health reminders \n";
+			$response .= "5. Ovulation Calculator \n";
+			$response .= "6. Due date calculator\n";
+			$response .= "7. Get instant ambulance for delivery \n";
+			$response .= "8. Checkout nearest health center \n";
+			$response .= "98. More \n";
+		break;
+
 		default:
 			$previous_menu = explode("*",$input);
 			array_pop($previous_menu);
 			//input_logic($previous_menu,$phoneNumber);
 			return "END An error occured";
 		break;
-	}
+	}*/
 
 	return $response;
 }
